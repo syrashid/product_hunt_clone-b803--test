bbae9fc103a5b0e97a0cf7312d3902e55b9c08c3
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-20 19:14:00 +0000

    TEST: Create first system test

---
+++ test/system/products_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit root_url # "/"
+    save_and_open_screenshot
+    assert_selector "h1", text: "Awesome Products"
+  end
+end
