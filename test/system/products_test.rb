d36c0c4d3442ed3683b3642687fc3d93936852b7
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-20 19:16:46 +0000

    FIX: Adjust code for system tests

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,6 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url # "/"
-    save_and_open_screenshot
     assert_selector "h1", text: "Awesome Products"
   end
 end
