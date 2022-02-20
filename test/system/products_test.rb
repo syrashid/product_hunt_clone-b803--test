9e3ebaf55dea792e790617b6aabeb3397fb483d3
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-20 19:16:58 +0000

    TEST: Make system test more robust

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,5 +4,6 @@ class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url # "/"
     assert_selector "h1", text: "Awesome Products"
+    assert_selector ".card-product", count: Product.count
   end
 end
